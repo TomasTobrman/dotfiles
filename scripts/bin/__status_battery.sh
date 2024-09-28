456eb89cbca333e33021208a591906f698a607f9
Tomáš Tobrman <30987354+TomasTobrman@users.noreply.github.com>
2024-09-28 10:42:04 +0200

    Battery status icons and colors

--- scripts/bin/__status_battery.sh
+++ scripts/bin/__status_battery.sh
@@ -3,23 +3,27 @@
 # Loop through all attached batteries.
 battery=/sys/class/power_supply/BAT0
 capacity="$(cat "$battery/capacity" 2>&1)"
-if [ "$capacity" -gt 90 ]; then
-	status=""
-elif [ "$capacity" -gt 60 ]; then
-	status=""
-elif [ "$capacity" -gt 40 ]; then
-	status=""
-elif [ "$capacity" -gt 10 ]; then
-	status=""
-else
-	status=""
+if [ "$capacity" -gt 90 ];   then status="󰁹"
+elif [ "$capacity" -gt 80 ]; then status="󰂂"
+elif [ "$capacity" -gt 70 ]; then status="󰂂"
+elif [ "$capacity" -gt 60 ]; then status="󰂀"
+elif [ "$capacity" -gt 50 ]; then status="󰁿"
+elif [ "$capacity" -gt 40 ]; then status="󰁾"
+elif [ "$capacity" -gt 30 ]; then status="󰁽"
+elif [ "$capacity" -gt 20 ]; then status="󰁼"
+elif [ "$capacity" -gt 10 ]; then status="󰁻"
+else                              status="󰁺";
 fi
 
-bg="#262626"
-fg="#ffffff"
+bg="#161616"
+fg="#525252"
 
 case "$(cat "$battery/status" 2>&1)" in
-	Full) status="" ;;
+	Full)
+		status="󰂄"
+		bg="#33b1ff"
+		fg="#161616"
+		;;
 	Discharging)
 		if [ "$capacity" -le 20 ]; then
 			status=" $status"
@@ -27,7 +31,11 @@ case "$(cat "$battery/status" 2>&1)" in
 			fg="#161616"
 		fi
 		;;
-	Charging) status="󰚥 $status" ;;
+	Charging)
+		status="󰚥 $status"
+		bg="#262626"
+		fg="#3ddbd9"
+		;;
 	"Not charging") status="" ;;
 	Unknown) status="? $status" ;;
 	*) exit 1 ;;
