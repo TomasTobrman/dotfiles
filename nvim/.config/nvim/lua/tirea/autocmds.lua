764414d0c05ec91ba516a780773b45adfa54a8c1
Tomáš Tobrman <30987354+TomasTobrman@users.noreply.github.com>
2024-10-15 15:38:14 +0200

    Nvim Shader filetypes

--- nvim/.config/nvim/lua/tirea/autocmds.lua
+++ nvim/.config/nvim/lua/tirea/autocmds.lua
@@ -6,6 +6,14 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	end,
 })
 
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	desc = "Set correct filetype to shader files",
+	pattern = { "*.vert", "*.frag" },
+	callback = function()
+		vim.opt_local.filetype = "glsl"
+	end,
+})
+
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	desc = "Expand Tab for filetypes",
 	pattern = { "c", "cpp", "haskell", "lhaskell" },
